1c29a7428fd8a45ec50b2b470de7801a300a11e5
user <user@user-pc>
2018-11-30 15:54:50 +0900

    asdf

--- GitTest/src/com/sist/hr/HelloGithup.java
+++ GitTest/src/com/sist/hr/HelloGithup.java
@@ -5,5 +5,6 @@ public class HelloGithup {
 		System.out.println("Hello Gitaegun");
 		System.out.println("Hell Githup");
 		System.out.println("fix query 3rd");
+		System.out.println("현석이혀응ㄴ 질문봇");
 	}
 }
