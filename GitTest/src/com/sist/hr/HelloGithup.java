45c67a0a5703e3e039f55a014490315c6450f3a5
sist1 <sist1@sist>
2018-11-30 15:57:19 +0900

    re fix

--- GitTest/src/com/sist/hr/HelloGithup.java
+++ GitTest/src/com/sist/hr/HelloGithup.java
@@ -2,9 +2,7 @@ package com.sist.hr;
 
 public class HelloGithup {
 	public static void main(String[] args) {
-		System.out.println("Hello Gitaegun");
-		System.out.println("Hell Githup");
-		System.out.println("fix query 3rd");
-		System.out.println("현석이혀응ㄴ 질문봇");
+		System.out.println("test 1st");
+
 	}
 }
