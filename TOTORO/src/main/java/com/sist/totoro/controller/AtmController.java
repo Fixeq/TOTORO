d0f833386a4291ebf081768aaa73af6cac45fc3d
TaeG <TaeG@DESKTOP-UJ1SHHI>
2018-12-10 21:14:58 +0900

    AtmSvc AtmController

--- TOTORO/src/main/java/com/sist/totoro/controller/AtmController.java
+++ TOTORO/src/main/java/com/sist/totoro/controller/AtmController.java
@@ -1,32 +1,69 @@
 package com.sist.totoro.controller;
 
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sist.totoro.common.SearchVO;
+import com.sist.totoro.domain.AtmVo;
+import com.sist.totoro.service.AtmSvc;
 
 @Controller
 public class AtmController {
 
 	private static final Logger logger = LoggerFactory.getLogger(AtmController.class);
 
+	@Autowired
+	private AtmSvc atmSvc;
+	
 	@RequestMapping(value = "/customerdeposit/customerdeposit.do", method = RequestMethod.GET)
-	public String customerdeposit(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-
+	public String customerdeposit(@ModelAttribute SearchVO invo,Model model) throws EmptyResultDataAccessException, ClassNotFoundException, SQLException {	
+		logger.info("SearchVO: "+invo);
+		//param -> view
+		
+		if(invo.getPage_size() == 0) {
+			invo.setPage_size(10);
+		}
+		
+		if(invo.getPage_num() == 0) {
+			invo.setPage_num(1);
+		}
+		
+		if(null == invo.getSearch_div()) {
+			invo.setSearch_div("");
+		}
+		
+		if(null == invo.getSearch_word()) {
+			invo.setSearch_word("");
+		}		
+		
+		
+		model.addAttribute("param",invo);
+		
+		List<AtmVo> list = atmSvc.do_retrieve(invo);
+		logger.info("list: "+list);
+		//총글수
+		int total_cnt = 0;
+		if(null != list && list.size()>0) {
+			total_cnt = list.get(0).getTotalCnt();
+			logger.info("total_cnt: "+total_cnt);
+		}
+		
+		
+		model.addAttribute("total_cnt",total_cnt);
+		model.addAttribute("list",list);
 		return "/atm/CustomerDeposit";
 	}
 	
--- TOTORO/src/main/java/com/sist/totoro/service/AtmSvcImple.java
+++ TOTORO/src/main/java/com/sist/totoro/service/AtmSvcImple.java
@@ -3,26 +3,24 @@ package com.sist.totoro.service;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sist.totoro.common.SearchVO;
+import com.sist.totoro.dao.AtmDao;
 import com.sist.totoro.domain.AtmVo;
 
+@Service
+public class AtmSvcImple implements AtmSvc {
 
+	@Autowired AtmDao atmDao;
+	
+	@Override
+	public List<AtmVo> do_retrieve(SearchVO searchVO)
+			throws ClassNotFoundException, SQLException, EmptyResultDataAccessException {
 
-public interface AtmSvc{
-   
-	/* (non-Javadoc)
-	 * @see com.sist.hr.user.service.UserSvc#do_retrieve(com.sist.hr.common.SearchVO)
-	 */
-	List<AtmVo> do_retrieve(SearchVO searchVO)
-			throws ClassNotFoundException, SQLException, EmptyResultDataAccessException;
+		return atmDao.do_retrieve(searchVO);
+	}
 
 }
-
-
-
